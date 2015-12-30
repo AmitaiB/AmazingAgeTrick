5a15d09f24f29507dcfae4e95f174388acfee846
AmitaiB <blickstein@gmail.com>
2015-12-29 19:39:44 -0500

    Initial commit (local)

---
+++ AmazingAgeTrickTests/AmazingAgeTrickTests.swift
@@ -0,0 +1,36 @@
+//
+//  AmazingAgeTrickTests.swift
+//  AmazingAgeTrickTests
+//
+//  Created by Amitai Blickstein on 12/29/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import AmazingAgeTrick
+
+class AmazingAgeTrickTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
