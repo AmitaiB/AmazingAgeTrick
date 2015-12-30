270143e6752b45286308faa77bf3ed6e8ccfa976
AmitaiB <blickstein@gmail.com>
2015-12-29 20:38:21 -0500

    Adds SwipeablecardView

---
+++ AmazingAgeTrick/ABSwipeableCardView.swift
@@ -0,0 +1,19 @@
+//
+//  ABSwipeableCardView.swift
+//  AmazingAgeTrick
+//
+//  Created by Amitai Blickstein on 12/29/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ABSwipeableCardView: UIView {
+    
+    var panGestureRecognizer: UIPanGestureRecognizer!
+    var originalPoint: CGPoint!
+    
+    
+    
+    func 
+}
