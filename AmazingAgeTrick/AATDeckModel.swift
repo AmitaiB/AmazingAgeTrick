6babe8fb4817e6a6cdce3454b9f2d9d7ded0bb85
AmitaiB <blickstein@gmail.com>
2016-01-05 22:12:45 -0500

    Impls cardInfoArray on ENUM - awesome

--- AmazingAgeTrick/AATDeckModel.swift
+++ AmazingAgeTrick/AATDeckModel.swift
@@ -7,28 +7,7 @@
 //
 
 /**
-TODO: Replace this all with an enum + function!!
-
-enum Suit {
-    case Spades, Hearts, Diamonds, Clubs
-    func simpleDescription() -> String {
-        switch self {
-        case .Spades:
-            return "spades"
-        case .Hearts:
-            return "hearts"
-        case .Diamonds:
-            return "diamonds"
-        case .Clubs:
-            return "clubs"
-        }
-    }
-}
-let hearts = Suit.Hearts
-let heartsDescription = hearts.simpleDescription()
-
-Excerpt From: Apple Inc. “The Swift Programming Language (Swift 2.1).” iBooks.
-
+TODO NEXT: Cascade using this throughout the app! VERY SWIFT! 👍
 */
 import UIKit
 
@@ -39,6 +18,27 @@ enum CardID:Int {
     case Card4 = 8
     case Card5 = 16
     case Card6 = 32
+    
+    func cardInfoArray()->[Int] {
+        switch self {
+        
+        case .Card1: return [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
+        case .Card2: return [2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31,34,35,38,39,42,43,46,47,50,51,54,55,58,59]
+        case .Card3: return [4,5,6,7,12,13,14,15,20,21,22,23,28,29,30,31,36,37,38,39,44,45,46,47,52,53,54,55,60]
+        case .Card4: return [8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31,40,41,42,43,44,45,46,47,56,57,58,59,60]
+        case .Card5: return [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,48,49,50,51,52,53,54,55,56,57,58,59,60]
+        case .Card6: return [32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
+        
+        }
+    }
+    
+    func cardTextForIndexPath(indexPath:NSIndexPath)->String {
+        if indexPath.row >= cardInfoArray().count {return ""}
+
+        let cardInfo = cardInfoArray()
+        return String(cardInfo[indexPath.row])
+    }
+    
 }
 
 class AATDeckModel: NSObject {
